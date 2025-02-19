fe0421511798c0443f5e68bb366af9562c9d061b
swami pedireddi <91956961+swamiped@users.noreply.github.com>
2025-02-19 05:55:30 +0530

    added comments into main.tf file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,17 +1,26 @@
 terraform{
     required_providers{
+        # installs the aws provider into the system
          aws = {
             source = "hashicorp/aws"
             version = "~> 5.84.0"
          }
     
     }
+
+    # we mentioned terraform required version here
     required_version = ">=v1.10.5"
     
 }
+
+# we are providing the region to create instance i.e. ap-south-1
 provider "aws" {
     region  = "ap-south-1"
 }
+
+# we are giving instance ami, tags and intance type to create a baisc ec2 instance
+# aws_instance is provider and resource type, myawsinstance is resourcename 
+
 resource "aws_instance" "myawsinstance" {
     
     ami = "ami-0ddfba243cbee3768"
